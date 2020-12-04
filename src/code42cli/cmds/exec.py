f7d5e063ef5bd9f795f43f99ecfae5f72d649499
Juliya Smith <juliya.smith@code42.com>
2020-12-04 16:59:03 +0000

    Eg

--- src/code42cli/cmds/exec.py
+++ src/code42cli/cmds/exec.py
@@ -8,8 +8,21 @@ command_argument = click.argument("cmd")
 @sdk_options(hidden=True)
 @command_argument
 def _exec(state, cmd):
-    """Execute arbitrary py42 code."""
+    """Execute arbitrary py42 code. Create a script using `sdk` 
+    as the py42 sdk to use your CLI connections in scripts easier.
+    
+    For example, in script.py:
+    
+        users_gen = sdk.users.get_all()
+        for page in users_gen:
+	        print(page)
+    
+    Then, using the CLI:
+    
+        code42 exec "$(cat script.py)"
 
+    """
+
+    # Assume sdk var used during exec()
     sdk = state.sdk
-    # Assume used below
     exec(cmd)
